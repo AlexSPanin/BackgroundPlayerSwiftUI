7fa962f6eb121c49321ea9803b838a0da07f61ba
Alexander Panin <panin1970@gmail.com>
2022-04-17 12:49:55 +0300

    Initial Commit

---
+++ BackgroundPlayerSwiftUI/BackgroundPlayerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundPlayerSwiftUIApp.swift
+//  BackgroundPlayerSwiftUI
+//
+//  Created by Александр Панин on 17.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundPlayerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
